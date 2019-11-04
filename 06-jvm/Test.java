f954857454ce027dc8da30331cb14ed7babb721f
gq13965786 <34830095+gq13965786@users.noreply.github.com>
2019-11-04 10:47:43 +0800

    Update Test.java

--- 06-jvm/Test.java
+++ 06-jvm/Test.java
@@ -6,9 +6,14 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String home = System.getenv("JAVA_HOME")+"/jre/bin/server/jvm.dll";
-		System.out.println(home);
-//		System.loadLibrary(home);
+		//String home = System.getenv("JAVA_HOME")+"/jre/bin/server/jvm.dll";replace / to \
+		//on the way to get 
+		
+		//JNIEXPORT jint JNICALL 
+		//Agent_OnLoad(JavaVM *vm, char *options, void *reserved)
+		
+		//System.out.println(home);
+		//System.loadLibrary(home);
 
 		int number = Runtime.getRuntime().availableProcessors();
 		System.out.println(number);
