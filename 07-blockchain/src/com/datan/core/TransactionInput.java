549040758bc80f206fee0006e543e6962112e6e9
gq13965786@live.co.uk <gq13965786@live.co.uk>
2019-11-12 16:56:09 +0800

    update

--- 07-blockchain/src/com/datan/core/TransactionInput.java
+++ 07-blockchain/src/com/datan/core/TransactionInput.java
@@ -1,5 +1,10 @@
 package com.datan.core;
 
 public class TransactionInput {
-
+	public String transactionOutputId;
+	public TransactionOutput UTXO;
+	
+	public TransactionInput(String transactionOutputId) {
+		this.transactionOutputId = transactionOutputId;
+	}
 }
--- 07-blockchain/src/com/datan/core/TransactionOutput.java
+++ 07-blockchain/src/com/datan/core/TransactionOutput.java
@@ -1,5 +1,25 @@
 package com.datan.core;
 
-public class TransactionOutput {
+import java.security.PublicKey;
 
+import com.datan.util.StringUtil;
+
+public class TransactionOutput {
+	public String id;
+	public PublicKey receiver;
+	public float value;
+	public String parentTransactionId;
+	
+	// Constructor
+	public TransactionOutput(PublicKey receiver, float value, String parentTransactionId) {
+		this.receiver = receiver;
+		this.value = value;
+		this.parentTransactionId = parentTransactionId;
+		this.id = StringUtil.applySha256(StringUtil.getStringFromKey(receiver) + Float.toString(value) + parentTransactionId);
+	}
+	
+	// Check if coin belongs to you
+	public boolean isMine(PublicKey publicKey) {
+		return (publicKey == receiver);
+	}
 }
